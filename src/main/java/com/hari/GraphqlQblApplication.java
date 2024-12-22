806cb450e3c54749d20b802a63c5323dd13095d3
mungsehari <mungse.hari@gmail.com>
2024-12-22 19:47:40 +0530

    create a graphql spring boot

---
+++ src/main/java/com/hari/GraphqlQblApplication.java
@@ -0,0 +1,13 @@
+package com.hari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlQblApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlQblApplication.class, args);
+	}
+
+}
